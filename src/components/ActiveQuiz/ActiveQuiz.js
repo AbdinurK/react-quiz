ab3fc50a1027bfd2bb4c3dfed9245ff045e2627a
Kuatbek Abdinur <kuatbekabdinur@gmail.com>
2020-07-26 18:28:14 +0600

    initial commit

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,27 @@
+import React from "react";
+import AnswersList from "./AnswersList/AnswersList";
+import "./ActiveQuiz.css"
+
+const ActiveQuiz = props => {
+    return (
+        <div className="ActiveQuiz">
+            <p className="Question">
+                <span>
+                    <strong>
+                        { props.answerNumber }.
+                    </strong>&nbsp;
+                    { props.question }
+                </span>
+                <small>{ props.answerNumber } из { props.quizLength }</small>
+            </p>
+            <AnswersList
+                answers={props.answers}
+                onAnswerClick={props.onAnswerClick}
+                state={props.state}
+            />
+        </div>
+    )
+};
+
+
+export default ActiveQuiz
